4d33fac736afdaf2e922db6fab4599ef3ca0df37
edwin.anton <edwin.anton@unmsm.edu.pe>
2025-04-13 23:14:43 +0000

    Mejoras en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,203 +4,124 @@ const Footer = () => {
       <footer className="w-full text-slate-400">
         <div className="pt-16 pb-12 text-sm border-t border-slate-900 bg-slate-800">
           <div className="container px-6 mx-auto">
-            <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
-              <nav
-                className="col-span-2 md:col-span-4 lg:col-span-3"
-                aria-labelledby="footer-product-dark"
-              >
-                <h3
-                  className="mb-6 text-base font-medium text-white"
-                  id="footer-product-dark"
-                >
-                  Product
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-8 lg:grid-cols-12">
+              {/* Logo y Descripción */}
+              <div className="col-span-1 md:col-span-8 lg:col-span-3">
+                <img src="https://uvsnieedcxndpdlyemgn.supabase.co/storage/v1/object/public/icons-psm//logo.png" alt="Voluntariado UNMSM" className="w-16 mb-4 rounded-md" />
+                <p className="mb-4">
+                  Estudiantes voluntarios impulsando el cambio social a través de la gestión de proyectos de alto impacto.
+                </p>
+              </div>
+              
+              {/* Enlaces Importantes */}
+              <nav className="col-span-1 md:col-span-4 lg:col-span-3" aria-labelledby="footer-links">
+                <h3 className="mb-6 text-base font-medium text-white" id="footer-links">
+                  Enlaces Importantes
                 </h3>
                 <ul>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Features
+                    <a href="/about" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      ¿Quiénes somos?
                     </a>
                   </li>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Customers
+                    <a href="/projects" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Proyectos
                     </a>
                   </li>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Why us?
+                    <a href="/volunteer" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Cómo ser voluntario
                     </a>
                   </li>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Pricing
+                    <a href="/faq" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Preguntas frecuentes
                     </a>
                   </li>
                 </ul>
               </nav>
-              <nav
-                className="col-span-2 md:col-span-4 lg:col-span-3"
-                aria-labelledby="footer-docs-dark"
-              >
-                <h3
-                  className="mb-6 text-base font-medium text-white"
-                  id="footer-docs-dark"
-                >
-                  Docs & help
+              
+              {/* Áreas de Trabajo */}
+              <nav className="col-span-1 md:col-span-4 lg:col-span-3" aria-labelledby="footer-areas">
+                <h3 className="mb-6 text-base font-medium text-white" id="footer-areas">
+                  Áreas de Trabajo
                 </h3>
                 <ul>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Documentation
+                    <a href="/talento-humano/capacitaciones" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Capacitaciones
                     </a>
                   </li>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Training
+                    <a href="/talento-humano/clima" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Clima
                     </a>
                   </li>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      System status
+                    <a href="/talento-humano/control" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Control
                     </a>
                   </li>
                   <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      FAQs
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Help Center
+                    <a href="/talento-humano/reclutamiento" className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600">
+                      Reclutamiento
                     </a>
                   </li>
                 </ul>
               </nav>
-              <nav
-                className="col-span-2 md:col-span-4 lg:col-span-3"
-                aria-labelledby="footer-about-dark"
-              >
-                <h3
-                  className="mb-6 text-base font-medium text-white"
-                  id="footer-about-dark"
-                >
-                  About us
+              
+              {/* Información de Contacto */}
+              <div className="col-span-1 md:col-span-8 lg:col-span-3">
+                <h3 className="mb-6 text-base font-medium text-white">
+                  Información de Contacto
                 </h3>
-                <ul>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      About us
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Careers
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Leadership
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Blog
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Events
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-              <nav
-                className="col-span-2 md:col-span-4 lg:col-span-3"
-                aria-labelledby="footer-get-in-touch-dark"
-              >
-                <h3
-                  className="mb-6 text-base font-medium text-white"
-                  id="footer-get-in-touch-dark"
-                >
-                  Get in touch
-                </h3>
-                <ul>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Contact
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Support
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Partners
-                    </a>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <a
-                      href="javascript:void(0)"
-                      className="transition-colors duration-300 hover:text-neutral-500 focus:text-neutral-600"
-                    >
-                      Join research
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+                
+                <div className="space-y-4">
+                  {/* Ubicación */}
+                  <div className="flex items-start">
+                    <div className="text-slate-400 mr-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                      </svg>
+                    </div>
+                    <div>
+                      <p className="text-sm text-slate-400">
+                        Ciudad Universitaria, UNMSM, Lima, Perú
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* Email */}
+                  <div className="flex items-start">
+                    <div className="text-slate-400 mr-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                      </svg>
+                    </div>
+                    <div>
+                      <p className="text-sm text-slate-400">
+                        voluntariado@unmsm.edu.pe
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* Teléfono */}
+                  <div className="flex items-start">
+                    <div className="text-slate-400 mr-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                      </svg>
+                    </div>
+                    <div>
+                      <p className="text-sm text-slate-400">
+                        (+51) 123-456-789
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
