32b86e62fb90102f5f304b0df19d4da39904c085
martinxanton <david.morillo1@unmsm.edu.pe>
2025-04-12 19:06:44 -0500

    Añadir rutas para las páginas de talento humano y manejar el error 404

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import LandingCapacitaciones from "../pages/LandingCapacitaciones";
+import LandingClima from "../pages/LandingClima";
+import LandingControl from "../pages/LandingControl";
+import LandingReclutamiento from "../pages/LandingReclutamiento";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+      <Route path="/talento-humano/capacitaciones" element={<LandingCapacitaciones/>} />
+      <Route path="/talento-humano/clima" element={<LandingClima/>} />
+      <Route path="/talento-humano/control" element={<LandingControl/>} />
+      <Route path="/talento-humano/reclutamiento" element={<LandingReclutamiento/>} />
     </Routes>
   );
 };
