798bb1c6517608a5efd0e6ffa7d9959009559328
edwin.anton <edwin.anton@unmsm.edu.pe>
2025-04-29 16:54:53 +0000

    update card

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -8,19 +8,21 @@ const CardProject = ({
   onButtonClick,
 }) => {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-md">
+    <div className="bg-white rounded-lg overflow-hidden shadow-md flex flex-col">
       {/* imgn proyecto: Recomendado 600x400px */}
-      <div className="aspect-[5/2]">
+      <div className="aspect-[5/2] overflow-hidden">
         <img
           src={imageUrl}
           alt={title}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover transition-transform duration-700 transform hover:scale-110"
         />
       </div>
 
-      <div className="p-6">
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
-        <p className="text-gray-700 mb-4">{description}</p>
+      <div className="p-6 grow flex flex-col justify-betwee">
+        <div>
+          <h3 className="text-xl font-semibold mb-2">{title}</h3>
+          <p className="text-gray-700 mb-4">{description}</p>
+        </div>
         <div className="flex justify-end">
           <button className="btn btn-link" onClick={onButtonClick}>
             {buttonText}
