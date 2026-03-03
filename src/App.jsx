dbf6b735a99718845e66d1f91cd968539c915718
martinxanton <edwin.anton.guerrero@gmail.com>
2026-03-02 22:17:26 -0500

    ajuste navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppRoutes from "./router/routes";
 
 const App = () => {
   return (
-    <div className="w-screen  overflow-x-hidden">
+    <div className="w-screen">
       <Navbar />
       <AppRoutes />
       <WhatsAppFAB />
